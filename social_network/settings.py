82e509a6f2f572fe67797033a9592970ef90e743
9rosLove <yarik2300@gmail.com>
2023-12-20 00:42:24 +0200

    add localhost to ALLOWED_HOSTS

--- social_network/settings.py
+++ social_network/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'django-insecure-@u)$wjwu=k#hqk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DJANGO_DEBUG", True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
