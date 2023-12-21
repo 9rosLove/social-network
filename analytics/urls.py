4b17fc7ae47cddbd2938aaf0ca7d86461ae5cfa0
9rosLove <yarik2300@gmail.com>
2023-12-21 14:01:31 +0200

    add pagination for LikeAnalyticsView

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from analytics.views import UserActivityView, AnalyticsView
+from analytics.views import UserActivityView, LikeAnalyticsView
 
 urlpatterns = [
-    path("", AnalyticsView.as_view()),
+    path("", LikeAnalyticsView.as_view()),
     path("user-activity/", UserActivityView.as_view()),
 ]
--- analytics/views.py
+++ analytics/views.py
@@ -1,10 +1,11 @@
 from datetime import timedelta, datetime
 
+from analytics.pagination import AnalyticsPagination
+from analytics.serializers import LikeAnalyticsSerializer
 from user.models import User
 from user.serializers import UserSerializer
 from network.models import Like
 
-from rest_framework.views import APIView
 from django.db.models import Count, F
 from rest_framework import status
 from rest_framework import generics
@@ -26,13 +27,16 @@ class UserActivityView(generics.RetrieveAPIView):
         return Response(activity)
 
 
-class AnalyticsView(APIView):
-    def get(self, request, *args, **kwargs):
-        date_from = request.query_params.get(
+class LikeAnalyticsView(generics.ListAPIView):
+    pagination_class = AnalyticsPagination
+    serializer_class = LikeAnalyticsSerializer
+
+    def get_queryset(self, *args, **kwargs):
+        date_from = self.request.query_params.get(
             "date_from",
             (datetime.now().date() + timedelta(days=-30)).isoformat(),
         )
-        date_to = request.query_params.get(
+        date_to = self.request.query_params.get(
             "date_to", datetime.now().date().isoformat()
         )
 
@@ -51,11 +55,18 @@ class AnalyticsView(APIView):
                 {"error": "Empty date"}, status=status.HTTP_400_BAD_REQUEST
             )
 
-        likes_count = (
+        queryset = (
             (Like.objects.filter(created_at__date__range=[date_from, date_to]))
             .values(date=F("created_at__date"))
             .order_by("-date")
             .annotate(likes_count=Count("id"))
         )
 
-        return Response(likes_count)
+        return queryset
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        page = self.paginate_queryset(serializer.data)
+
+        return self.get_paginated_response(page)
